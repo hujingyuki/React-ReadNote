e84a1424fae32b607ee9ac797e0645fbddda8130
hujingyuki <hujingyuki@126.com>
2019-12-24 00:05:37 +0800

    一级渲染

--- test/button.js
+++ test/button.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 class Button extends React.Component {
   render () {
-    return <div />
+    return <button> {this.props.text} </button>
   }
 }
 
--- test/button.spec.js
+++ test/button.spec.js
@@ -1,7 +1,15 @@
 import React from 'react'
-import TestUtils from 'react-dom/test-utils'
+import TestRenderer from 'react-test-renderer'
 import Button from './button'
 
 test('works', () => {
   expect(true).toBe(true)
+})
+
+test('renders with text', () => {
+  const text = '123'
+  const testRenderer = TestRenderer.create(<Button text={text} />)
+  const testInstance = testRenderer.root
+  expect(testInstance.props.text).toBe(text)
+  // expect(testInstance.type).toBe('[Function Button]')
 })
